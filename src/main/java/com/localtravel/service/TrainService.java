5c1bfa6a84979c2e666476c32e5e6cbfc4a72d28
leeyeonwoo <eto3e40@gmail.com>
2023-01-27 16:12:42 +0900

    01.27.01 yeonwoo
    TR클래스 작업
    - 역 스케줄 출력, DB 출력 가능
    - 구간별 가격 출력 진행 중
    - 테스트용 JSP파일 생성, testPage에 저장
    - 구간별 요금 저장을 위한 csv 파일 저장

--- src/main/java/com/localtravel/service/TrainService.java
+++ src/main/java/com/localtravel/service/TrainService.java
@@ -1,12 +1,18 @@
 package com.localtravel.service;
 
 import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.UnsupportedEncodingException;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.nio.charset.Charset;
 import java.net.URLEncoder;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -163,6 +169,10 @@ public class TrainService {
 
 	// 3. 열차 스케줄 가져오기 ( 예매시 사용할 스케쥴 모음 )
 	public String searchSchedule(TRInputScheduleDto inputSchedule) throws Exception {
+		// 1. 입력한 역의 이름을 역코드로 변환해준다. ( => SERVICE 3.1.  )
+		changeStaionId(inputSchedule);
+		
+		// 2. 출/도착역의 정보를 입력 뒤 API를 통해 스케줄을 가져온다.
 		StringBuilder urlBuilder = new StringBuilder("http://apis.data.go.kr/1613000/TrainInfoService/getStrtpntAlocFndTrainInfo"); /*URL*/
         urlBuilder.append("?" + URLEncoder.encode("serviceKey","UTF-8") + "=rcDIgqtDvPLJ95VNJCSK44R1cExWDo42RSBvLWAV9IglwBSjU5fsECwXMglNDmsEk%2BxoZypGMP1kn72z0jXbcw%3D%3D"); /*Service Key*/
         urlBuilder.append("&" + URLEncoder.encode("pageNo","UTF-8") + "=" + URLEncoder.encode("1", "UTF-8")); //페이지번호
@@ -190,10 +200,72 @@ public class TrainService {
         }
         rd.close();
         conn.disconnect();
-        System.out.println(sb.toString());
+//      System.out.println(sb.toString());
 		
+		Gson gson = new Gson();
+		JsonElement element = JsonParser.parseString(sb.toString());
+		JsonObject body = element.getAsJsonObject().get("response").getAsJsonObject().get("body").getAsJsonObject();
+		JsonArray ScheduleArray = body.get("items").getAsJsonObject().get("item").getAsJsonArray();
+		System.out.println(ScheduleArray.get(0));
+		return null;
+	}
+	
+	// 3.1. API양식에 맞게 입력한 역의 이름을 역코드로 변환 및 날짜 수정
+	private TRInputScheduleDto changeStaionId(TRInputScheduleDto inputSchedule) {
+		
+		// 3.1.1. 출발역의 코드를 찾아준다.
+		String depId = trdao.selectStationId(inputSchedule.getDepPlaceId());
+		System.out.println("찾은 출발역의 코드 : " + depId);
+		
+		// 3.1.2. 도착역의 코드를 찾아준다.
+		String arrId = trdao.selectStationId(inputSchedule.getArrPlaceId());
+		System.out.println("찾은 도착역의 코드 : " + arrId);
+		
+		//3.1.3. 날짜데이터의 "-" 제거
+		
+		String setTime = inputSchedule.getDepPlandTime().replace("-", "");
+		
+		// 3.1.4. 각 코드를 Dto에 통해준다.
+		inputSchedule.setDepPlaceId(depId);
+		inputSchedule.setArrPlaceId(arrId);
+		inputSchedule.setDepPlandTime(setTime);
+		System.out.println("수정된 입력 정보 : " + inputSchedule);
 		
 		return null;
 	}
-
+	
+	public void fareCsv() {
+		List<List<String>> ret = new ArrayList<List<String>>();
+        BufferedReader br = null;
+        
+        try{
+            br = Files.newBufferedReader(Paths.get("C:\\SpringWorkspace\\LocalTravel\\src\\test\\resources\\ktxFare.csv"));
+            Charset.forName("UTF-8");
+            String line = "";
+            
+            while((line = br.readLine()) != null){
+            	System.out.println("line : " + line);
+                //CSV 1행을 저장하는 리스트
+                List<String> tmpList = new ArrayList<String>();
+                String array[] = line.split(",");
+                //배열에서 리스트 반환
+                tmpList = Arrays.asList(array);
+                System.out.println(tmpList);
+                ret.add(tmpList);
+            }
+        }catch(FileNotFoundException e){
+            e.printStackTrace();
+        }catch(IOException e){
+            e.printStackTrace();
+        }finally{
+            try{
+                if(br != null){
+                    br.close();
+                }
+            }catch(IOException e){
+                e.printStackTrace();
+            }
+        }
+	}
+	
 }
